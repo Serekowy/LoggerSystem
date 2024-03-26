241c66df035de06786a9faef76b73b9e70aa0a75
Cheesy2137 <adrian997112999@gmail.com>
2024-03-26 21:11:18 +0100

    continue writing tests

--- src/test/java/LoggerSystemTest.java
+++ src/test/java/LoggerSystemTest.java
@@ -4,6 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoggerSystemTest {
     @Test
+    void addUserShouldAddNewUser() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        assertTrue(loggerSystem.addUser(new User("User", AccessType.BASIC)));
+    }
+    @Test
+    void addUserShouldNotAddTheSameUserName() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        loggerSystem.addUser(new User("User", AccessType.ADMIN));
+        assertFalse(loggerSystem.addUser(new User("User", AccessType.BASIC)));
+    }
+    @Test
+    void getUserLogsShouldNotShowWhenUserNotExist() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+
+        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+
+        assertTrue(loggerSystem.getUserLogs("User", "").isEmpty());
+
+    }
+    @Test
+    void getUserLogsShouldShowWhenUserExist() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        loggerSystem.addUser(new User("User", AccessType.BASIC));
+        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+
+        assertFalse(loggerSystem.getUserLogs("User", "").isEmpty());
+
+    }
+    @Test
     void removeLogShouldRemoveLog() {
         LoggerSystem loggerSystem = new LoggerSystem();
         loggerSystem.addUser(new User("User", AccessType.BASIC));
@@ -14,21 +43,54 @@ class LoggerSystemTest {
 
     }
     @Test
-    void addLogShouldNotAddWhenUserNotExist() {
-        LoggerSystem loggerSystem = new LoggerSystem();
-
-        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
-
-        assertTrue(loggerSystem.getUserLogs("User", "").isEmpty());
-
-    }
-    @Test
-    void addLogShouldAddWhenUserExist() {
+    void removeLogShouldNotRemoveLogWhenEmpty() {
         LoggerSystem loggerSystem = new LoggerSystem();
         loggerSystem.addUser(new User("User", AccessType.BASIC));
         loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+        loggerSystem.removeLog("", "", 0);
+
+        assertFalse(loggerSystem.getUserLogs("User", "").isEmpty());
+    }
+    @Test
+    void removeLogShouldNotRemoveLogWhenWrongNumberGiven() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        loggerSystem.addUser(new User("User", AccessType.BASIC));
+        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+        loggerSystem.removeLog("User", "", 5);
 
         assertFalse(loggerSystem.getUserLogs("User", "").isEmpty());
 
     }
+    @Test
+    void removeLogShouldNotRemoveLogWhenMinusNumberGiven() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        loggerSystem.addUser(new User("User", AccessType.BASIC));
+        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+        loggerSystem.removeLog("User", "", -5);
+
+        assertFalse(loggerSystem.getUserLogs("User", "").isEmpty());
+
+    }
+    @Test
+    void removeLogShouldNotRemoveLogWhenOnlyUserToDeleteGiven() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        loggerSystem.addUser(new User("User", AccessType.BASIC));
+        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+        loggerSystem.removeLog("", "User", 0);
+
+        assertFalse(loggerSystem.getUserLogs("User", "").isEmpty());
+
+    }
+    @Test
+    void removeLogShouldRemoveOtherUserLog() {
+        LoggerSystem loggerSystem = new LoggerSystem();
+        loggerSystem.addUser(new User("User", AccessType.BASIC));
+        loggerSystem.addUser(new User("User2", AccessType.ADMIN));
+        loggerSystem.addLog(new LogSystem("User", "now", "jakis tekst log", "text"));
+        loggerSystem.removeLog("User2", "User", 0);
+
+        assertTrue(loggerSystem.getUserLogs("User", "").isEmpty());
+
+    }
+
 }
